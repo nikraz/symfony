a04b08c9b63643cf0b8c24e94ee29c1f3b004806
nikolay razlozhki <nikolay.r@invest.com>
2018-07-31 17:50:08 +0300

    uploading blog

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends Controller
+{
+    /** @var int */
+    const POST_LIMIT = 2;
+
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $authorRepository;
+
+    /** @var \Doctrine\Common\Persistence\ObjectRepository */
+    private $blogPostRepository;
+
+    /**
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->blogPostRepository = $entityManager->getRepository('App:BlogPost');
+        $this->authorRepository = $entityManager->getRepository('App:Author');
+    }
+
+    /**
+     * @Route("/", name="homepage")
+     * @Route("/entries", name="entries")
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \LogicException
+     */
+    public function entriesAction(Request $request)
+    {
+        $page = 1;
+
+        if ($request->get('page')) {
+            $page = $request->get('page');
+        }
+
+        return $this->render(
+            'blog/entries.html.twig',
+            [
+                'blogPosts' => $this->blogPostRepository->getAllPosts($page, self::POST_LIMIT),
+                'totalBlogPosts' => $this->blogPostRepository->getPostCount(),
+                'page' => $page,
+                'entryLimit' => self::POST_LIMIT,
+            ]
+        );
+    }
+
+    /**
+     * @Route("/entry/{slug}", name="entry")
+     */
+    public function entryAction($slug)
+    {
+        $blogPost = $this->blogPostRepository->findOneBySlug($slug);
+
+        if (!$blogPost) {
+            $this->addFlash('error', 'Unable to find entry!');
+
+            return $this->redirectToRoute('entries');
+        }
+
+        return $this->render(
+            'blog/entry.html.twig',
+            [
+                'blogPost' => $blogPost,
+            ]
+        );
+    }
+
+    /**
+     * @Route("/author/{name}", name="author")
+     */
+    public function authorAction($name)
+    {
+        $author = $this->authorRepository->findOneByUsername($name);
+
+        if (!$author) {
+            $this->addFlash('error', 'Unable to find author!');
+
+            return $this->redirectToRoute('entries');
+        }
+
+        return $this->render(
+            'blog/author.html.twig',
+            [
+                'author' => $author,
+            ]
+        );
+    }
+}
